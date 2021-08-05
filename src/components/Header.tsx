936202488c1bd744e0c62cc3fa5bca82342d28da
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-08-04 21:25:55 -0300

    add icons and avatar to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react";
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from "react-icons/ri";
 
 export function Header() {
   return(
@@ -44,6 +44,30 @@ export function Header() {
           _placeholder={{color: 'gray.400'}}
         />
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20"/>
+          <Icon as={RiUserAddLine} fontSize="20"/>
+        </HStack>
+
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Camila Miz</Text>
+            <Text color="gray.300" fontSize="small">
+              camila@gmail.com
+            </Text>
+          </Box>
+          <Avatar size="md" name="Camila Miz" src="https://www.github.com/camilamiz.png"/>
+        </Flex>
       </Flex>
     </Flex>
   );
